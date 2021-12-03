7df823ccc2a5383b55239dfc652cbda5fa864480
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-12-03 23:10:46 +0100

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
+import React, { useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import Home from "./pages/home";
+
 import "./App.css";
 
 const App = () => {
-  return <h1>warehouse-ui</h1>;
+  const [application] = useState({
+    name: "Warehouse",
+    pages: {
+      home: { name: "Főoldal", path: "/" },
+    },
+  });
+
+  const { pages } = application;
+  const { home } = pages;
+
+  return (
+    <BrowserRouter>
+      <main className="container">
+        <Switch>
+          <Route exact path={home.path} component={Home} />
+        </Switch>
+      </main>
+    </BrowserRouter>
+  );
 };
 
 export default App;
