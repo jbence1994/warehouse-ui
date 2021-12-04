78decc04467ba12f2255ce654894b1f0f42d76fb
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-12-04 13:23:06 +0100

    Create a basic skeleton to page

--- src/pages/supplies.jsx
+++ src/pages/supplies.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Supplies = () => {
-  return <h1>Supplies</h1>;
+  return (
+    <React.Fragment>
+      <section className="row">
+        <article className="col-sm-12 col-md-12 col-lg-4">
+          <h1>Raktárkészlet</h1>
+        </article>
+      </section>
+    </React.Fragment>
+  );
 };
 
 export default Supplies;
