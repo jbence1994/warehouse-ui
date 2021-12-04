756b1829d6a254f8304e7e13a71fb36a5ac9e405
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2021-12-04 13:13:21 +0100

    Create and configure not found page for application's paths (#3)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import Navbar from "./components/common/navbar";
+import NotFound from "./pages/notFound";
 import Home from "./pages/home";
 
 import "./App.css";
@@ -11,19 +12,22 @@ const App = () => {
     name: "Warehouse",
     pages: {
       home: { name: "Főoldal", path: "/" },
+      notFound: { name: "A keresett oldal nem található", path: "/hiba" },
     },
   });
 
   const { name: applicationName } = application;
   const { pages } = application;
-  const { home } = pages;
+  const { home, notFound } = pages;
 
   return (
     <BrowserRouter>
       <Navbar title={applicationName} homeRoute={home.path} routes={[]} />
       <main className="container">
         <Switch>
+          <Route exact path={notFound.path} component={NotFound} />
           <Route exact path={home.path} component={Home} />
+          <Redirect to={notFound.path} />
         </Switch>
       </main>
     </BrowserRouter>
---
+++ src/pages/notFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotFound = () => {
+  return <h1>A keresett oldal nem található!</h1>;
+};
+
+export default NotFound;
