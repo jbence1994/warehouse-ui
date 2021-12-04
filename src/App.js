8067e335ebfb242142b6ad25dfaa3e5e3abed318
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-12-04 13:18:32 +0100

    Configure supplies page to routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import Navbar from "./components/common/navbar";
+import Supplies from "./pages/supplies";
 import NotFound from "./pages/notFound";
 import Home from "./pages/home";
 
@@ -12,19 +13,21 @@ const App = () => {
     name: "Warehouse",
     pages: {
       home: { name: "Főoldal", path: "/" },
+      supplies: { name: "Raktárkészlet", path: "/raktarkeszlet" },
       notFound: { name: "A keresett oldal nem található", path: "/hiba" },
     },
   });
 
   const { name: applicationName } = application;
   const { pages } = application;
-  const { home, notFound } = pages;
+  const { home, supplies, notFound } = pages;
 
   return (
     <BrowserRouter>
       <Navbar title={applicationName} homeRoute={home.path} routes={[]} />
       <main className="container">
         <Switch>
+          <Route exact path={supplies.path} component={Supplies} />
           <Route exact path={notFound.path} component={NotFound} />
           <Route exact path={home.path} component={Home} />
           <Redirect to={notFound.path} />
