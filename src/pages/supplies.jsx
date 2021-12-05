3e7a25b4b97cac586f2d406322b8f4d56e7f6a62
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2021-12-05 19:18:44 +0100

    Implement supplies page (#4)
    * Create supplies page
    
    * Configure supplies page to routes
    
    * Render page names on navbar
    
    * Create a basic skeleton to page
    
    * Install axios
    
    * Render products in a card component
    
    * Move hard-coded data to service
    
    * Fetch supplies from api

---
+++ src/pages/supplies.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+import Card from "../components/common/card";
+
+import { getSupplies } from "../services/supplyService";
+
+const Supplies = () => {
+  const [supplies, setSupplies] = useState([]);
+
+  useEffect(() => {
+    const populateSupplies = async () => {
+      const { data } = await getSupplies();
+      setSupplies(data);
+    };
+    populateSupplies();
+  }, []);
+
+  return (
+    <React.Fragment>
+      <section className="row">
+        {supplies.map(({ id, product, quantity }) => (
+          <article key={id} className="col-sm-12 col-md-12 col-lg-4">
+            <Card
+              title={product.name}
+              // TODO: call api to fetch photo of a product.
+              imageSource="https://picsum.photos/500"
+              imageAlt={product.name}
+              contents={[
+                { key: 1, value: `Ár: ${product.price}.- Forint` },
+                { key: 2, value: `Egység: ${product.unit}` },
+                { key: 3, value: `Kereskedő: ${product.merchant.name}` },
+                {
+                  key: 4,
+                  // TODO: after supply quantity update re-render quantity.
+                  value: `Jelenleg raktáron: ${quantity} ${product.unit}`,
+                },
+              ]}
+            />
+          </article>
+        ))}
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default Supplies;
