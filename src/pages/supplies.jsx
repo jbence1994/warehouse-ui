323502ba5756ab7653e2a678152ed1b76d21e31f
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-12-12 12:57:46 +0100

    Call for all products properly

--- src/pages/supplies.jsx
+++ src/pages/supplies.jsx
@@ -3,47 +3,49 @@ import React, { useEffect, useState } from "react";
 import Card from "../components/common/card";
 import AddSupplyEntryForm from "../components/addSupplyEntryForm";
 
-import { getSupplies } from "../services/supplyService";
+import { getProducts } from "../services/productService";
 
 const Supplies = () => {
-  const [supplies, setSupplies] = useState([]);
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    const populateSupplies = async () => {
-      const { data } = await getSupplies();
-      setSupplies(data);
+    const populateProducts = async () => {
+      const { data } = await getProducts();
+      setProducts(data);
     };
-    populateSupplies();
+    populateProducts();
   }, []);
 
   return (
     <React.Fragment>
       <section className="row">
-        {supplies.map(({ id, product, quantity }) => (
-          <article key={id} className="col-sm-12 col-md-12 col-lg-4">
-            <Card
-              title={product.name}
-              // TODO: call api to fetch photo of a product.
-              imageSource="https://picsum.photos/500"
-              imageAlt={product.name}
-              contents={[
-                { key: 1, value: `Ár: ${product.price}.- Forint` },
-                { key: 2, value: `Egység: ${product.unit}` },
-                { key: 3, value: `Kereskedő: ${product.merchant.name}` },
-                {
-                  key: 4,
-                  // TODO: after supply quantity update re-render quantity.
-                  value: `Jelenleg raktáron: ${quantity} ${product.unit}`,
-                },
-              ]}
-            />
-            <AddSupplyEntryForm
-              name="quantity"
-              productId={product.id}
-              content={product.unit}
-            />
-          </article>
-        ))}
+        {products.map(
+          ({ id, name, price, unit, availableQuantity, merchant }) => (
+            <article key={id} className="col-sm-12 col-md-12 col-lg-4">
+              <Card
+                title={name}
+                // TODO: call api to fetch photo of a product.
+                imageSource="https://picsum.photos/500"
+                imageAlt={name}
+                contents={[
+                  { key: 1, value: `Ár: ${price}.- Forint` },
+                  { key: 2, value: `Egység: ${unit}` },
+                  { key: 3, value: `Kereskedő: ${merchant.name}` },
+                  {
+                    key: 4,
+                    // TODO: after supply quantity update re-render quantity.
+                    value: `Jelenleg raktáron: ${availableQuantity} ${unit}`,
+                  },
+                ]}
+              />
+              <AddSupplyEntryForm
+                name="quantity"
+                productId={id}
+                content={unit}
+              />
+            </article>
+          )
+        )}
       </section>
     </React.Fragment>
   );
