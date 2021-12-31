d65e31232531744f9f5653f00f3d2c249e503682
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-12-31 16:16:05 +0100

    Size photos to equal

--- src/components/common/card.jsx
+++ src/components/common/card.jsx
@@ -3,7 +3,18 @@ import React from "react";
 const Card = ({ title, imageSource, imageAlt, contents }) => {
   return (
     <div className="card mt-3 mb-3">
-      <img src={imageSource} className="card-img-top" alt={imageAlt} />
+      <img
+        src={imageSource}
+        className="card-img-top"
+        style={{
+          width: "250px",
+          height: "250px",
+          display: "block",
+          margin: "0 auto",
+          "object-fit": "fill",
+        }}
+        alt={imageAlt}
+      />
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         {contents.map((content) => (
