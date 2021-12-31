05f960df0b94e301cc1228e3d8dd4ca543deb92a
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-12-31 17:04:43 +0100

    If there is no photo available, display a "no-image" photo

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -4,9 +4,15 @@ import Card from "../components/common/card";
 
 import endpoints from "../api.endpoints.json";
 
+import noImage from "../components/no-image.png";
+
 const ProductCard = ({ name, price, merchantName, photoFileName }) => {
   const { base, getProductPhoto } = endpoints;
-  const imageSource = `${base}${getProductPhoto}${photoFileName}`;
+  let imageSource = noImage;
+
+  if (photoFileName) {
+    imageSource = `${base}${getProductPhoto}${photoFileName}`;
+  }
 
   return (
     <React.Fragment>
