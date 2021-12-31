04b86b9c02e7275d7a9682214ebfe209e3f7ece2
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2021-12-31 18:23:39 +0100

    Implement skeleton of the request

--- src/components/uploadPhotoForm.jsx
+++ src/components/uploadPhotoForm.jsx
@@ -2,31 +2,34 @@ import React from "react";
 
 import Submit from "./common/submit";
 
-// TODO: import { uploadPhoto } from "../services/productPhotoService";
+import { uploadPhoto } from "../services/productPhotoService";
 
 const UploadPhotoForm = ({ productId }) => {
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(`TODO: Send photo of product #${productId} to server.`);
+    console.log("Hello");
+    const { data } = await uploadPhoto(productId, null); //TODO: get photo obj.
+
+    console.log(data);
   };
 
   return (
     <form onSubmit={handleSubmit} noValidate>
       <div
-        style={{
+      /*style={{
           position: "relative",
           overflow: "hidden",
-        }}
+        }}*/
       >
         <input
           type="file"
-          style={{
+          /*style={{
             fontSize: "100px",
             position: "absolute",
             left: "0",
             top: "0",
             opacity: "0",
-          }}
+          }}*/
         />
         <Submit text="Kép feltöltése" />
       </div>
