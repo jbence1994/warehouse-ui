6a8b376230f02a8375ab9e34d3e8d58be0a6bde5
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-08 16:38:24 +0100

    Create Products page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import Navbar from "./components/common/navbar";
+import Products from "./pages/products";
 import Supplies from "./pages/supplies";
 import NotFound from "./pages/notFound";
 import Home from "./pages/home";
@@ -14,21 +15,26 @@ const App = () => {
     pages: {
       home: { name: "Főoldal", path: "/" },
       supplies: { name: "Raktárkészlet", path: "/raktarkeszlet" },
+      products: { name: "Termékek", path: "/termekek" },
       notFound: { name: "A keresett oldal nem található", path: "/hiba" },
     },
   });
 
   const { name: applicationName } = application;
   const { pages } = application;
-  const { home, supplies, notFound } = pages;
+  const { home, supplies, products, notFound } = pages;
 
-  const routes = [{ name: supplies.name, path: supplies.path }];
+  const routes = [
+    { name: supplies.name, path: supplies.path },
+    { name: products.name, path: products.path },
+  ];
 
   return (
     <BrowserRouter>
       <Navbar title={applicationName} homeRoute={home.path} routes={routes} />
       <main className="container">
         <Switch>
+          <Route exact path={products.path} component={Products} />
           <Route exact path={supplies.path} component={Supplies} />
           <Route exact path={notFound.path} component={NotFound} />
           <Route exact path={home.path} component={Home} />
