1dc0f87daa374ef026fa37150ed47e98cea9e67e
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-09 14:52:24 +0100

    Display "no image"

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -4,22 +4,26 @@ import Card from "../components/common/card";
 
 import endpoints from "../config/api.endpoints";
 
+import noImage from "../images/no-image.png";
+
 const ProductCard = ({ name, price, merchantName, photoFileName }) => {
   const { BASE, PRODUCT_PHOTOS, SLASH } = endpoints;
-  const imageSource = `${BASE}${PRODUCT_PHOTOS}${SLASH}${photoFileName}`;
+  let imageSource = noImage;
+
+  if (photoFileName) {
+    imageSource = `${BASE}${PRODUCT_PHOTOS}${SLASH}${photoFileName}`;
+  }
 
   return (
-    <React.Fragment>
-      <Card
-        title={name}
-        imageSource={imageSource}
-        imageAlt={name}
-        contents={[
-          { key: 1, value: `Ár: ${price}.- Forint` },
-          { key: 2, value: `Kereskedő: ${merchantName}` },
-        ]}
-      />
-    </React.Fragment>
+    <Card
+      title={name}
+      imageSource={imageSource}
+      imageAlt={name}
+      contents={[
+        { key: 1, value: `Ár: ${price}.- Forint` },
+        { key: 2, value: `Kereskedő: ${merchantName}` },
+      ]}
+    />
   );
 };
 
