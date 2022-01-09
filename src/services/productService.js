0609fc026c093cbc085522fd52b22dab99a590c8
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-09 13:56:16 +0100

    Add createProduct function

--- src/services/productService.js
+++ src/services/productService.js
@@ -3,17 +3,26 @@ import restClient from "axios";
 import endpoints from "../config/api.endpoints.json";
 
 const getProducts = async (includeMerchants = true) => {
-  const { base, getProducts, includeMerchantWithProducts } = endpoints;
+  const { base, getProductsOrCreateProduct, includeMerchantWithProducts } =
+    endpoints;
 
   let endpointToCall = base;
 
   if (includeMerchants) {
-    endpointToCall += `${getProducts}${includeMerchantWithProducts}`;
+    endpointToCall += `${getProductsOrCreateProduct}${includeMerchantWithProducts}`;
   } else {
-    endpointToCall += `${getProducts}`;
+    endpointToCall += `${getProductsOrCreateProduct}`;
   }
 
   return await restClient.get(endpointToCall);
 };
 
-export { getProducts };
+const createProduct = async (product) => {
+  const { base, getProductsOrCreateProduct } = endpoints;
+
+  const endpointToCall = `${base}${getProductsOrCreateProduct}`;
+
+  return await restClient.post(endpointToCall, product);
+};
+
+export { getProducts, createProduct };
