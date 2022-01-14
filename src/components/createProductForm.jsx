2d181453c76eec78f1f4b74cd6de9e26fd62da36
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-14 16:19:03 +0100

    Add base implementation (need to fix)

--- src/components/createProductForm.jsx
+++ src/components/createProductForm.jsx
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 import TextInput from "./common/textInput";
 import NumberInput from "./common/numberInput";
 import MerchantsDropdown from "./merchantsDropdown";
 import Submit from "./common/submit";
 
+import { createProduct } from "../services/productService";
+
 const CreateProductForm = () => {
-  const handleSubmit = (e) => {
+  const [product, setProduct] = useState({
+    name: "",
+    unit: "",
+    price: 0,
+    merchantId: 0,
+  });
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log("Send new product to api.");
+    const { data } = await createProduct(product);
+
+    console.log(data);
   };
 
   const handleTextChange = ({ currentTarget: input }) => {
-    console.log("products.handleTextChange.", input.name, input.value);
+    const updatedProduct = { ...product };
+    updatedProduct[input.name] = input.value;
+    setProduct(updatedProduct);
   };
 
   const handleNumberChange = ({ currentTarget: input }) => {
-    console.log("products.handleNumberChange.", input.name, input.value);
+    const updatedProduct = { ...product };
+    updatedProduct[input.name] = parseInt(input.value);
+    setProduct(updatedProduct);
   };
 
   return (
     <React.Fragment>
+      <h3>
+        <code>{JSON.stringify(product)}</code>
+      </h3>
       <form onSubmit={handleSubmit} noValidate>
         <TextInput
           labelText="Termék neve"
