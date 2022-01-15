8199bd1ae1e1389b200a9ccd6ed9c155fcfa21cd
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-15 08:21:52 +0100

    Fix bug

--- src/services/merchantService.js
+++ src/services/merchantService.js
@@ -3,9 +3,9 @@ import restClient from "axios";
 import endpoints from "../config/api.endpoints";
 
 const getMerchantKeyValuePairs = async () => {
-  const { API_ROOT, MERCHANTS, AS_KEY_VALUE_PAIRS } = endpoints;
+  const { API_ROOT, MERCHANTS, AS_KEY_VALUE_PAIR } = endpoints;
 
-  const endpointToCall = `${API_ROOT}${MERCHANTS}${AS_KEY_VALUE_PAIRS}`;
+  const endpointToCall = `${API_ROOT}${MERCHANTS}${AS_KEY_VALUE_PAIR}`;
 
   return await restClient.get(endpointToCall);
 };
