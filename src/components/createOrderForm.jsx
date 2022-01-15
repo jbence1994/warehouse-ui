0ce5a749147d3d2f596898969ecd93d2597d461a
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-15 07:02:49 +0100

    Add createOrderForm component

---
+++ src/components/createOrderForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { createOrder } from "../services/orderService";
+
+const CreateOrderForm = () => {
+  return <h1>CreateOrderForm</h1>;
+};
+
+export default CreateOrderForm;
