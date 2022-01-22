85cb4d5f4ca4032d2662737ccb5ca0e442a1919d
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-22 16:28:47 +0100

    Design an error page

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 
+import RoutingButton from "../components/common/routingButton";
+
 const Error = () => {
-  return <h1>Error page</h1>;
+  return (
+    <React.Fragment>
+      <div className="jumbotron">
+        <h1 className="display-4">Hoppá!</h1>
+        <p className="lead">Hiba történt a hálózati kapcsolatban...</p>
+        <hr className="my-4" />
+        <p>
+          Próbálkozzon később újraindítani az oldalt vagy ellenőrízze az
+          internetkapcsolatot.
+        </p>
+        <p className="lead">
+          <RoutingButton text="Vissza a főoldalra" url="/" />
+        </p>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default Error;
