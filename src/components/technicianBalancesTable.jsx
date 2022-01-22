dc6d52c88d7c4f4548e35198222933fa91a94f67
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-22 13:59:07 +0100

    Fetch balances

--- src/components/technicianBalancesTable.jsx
+++ src/components/technicianBalancesTable.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Table from "./common/table";
 
+import { getTechnicianBalances } from "../services/technicianBalanceService";
+
 const TechnicianOrdersTable = ({ technicianId }) => {
+  const [technicianBalances, setTechnicianBalances] = useState([]);
+
+  useEffect(() => {
+    const populateTechnicianBalances = async (id) => {
+      const { data } = await getTechnicianBalances(id);
+      setTechnicianBalances(data);
+    };
+
+    populateTechnicianBalances(technicianId);
+  }, [technicianId]);
+
   const columns = [
     { key: 1, value: "Azonosító" },
     { key: 2, value: "Dátum" },
     { key: 3, value: "Összeg" },
   ];
 
-  return <Table collection={[]} columns={columns} />;
+  return <Table collection={technicianBalances} columns={columns} />;
 };
 
 export default TechnicianOrdersTable;
