a55dd14ba4b6dec9ec13e4f8081ad3d8945175fa
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-22 16:32:56 +0100

    Remove React fragment

--- src/pages/error.jsx
+++ src/pages/error.jsx
@@ -4,20 +4,18 @@ import RoutingButton from "../components/common/routingButton";
 
 const Error = () => {
   return (
-    <React.Fragment>
-      <div className="jumbotron">
-        <h1 className="display-4">Hoppá!</h1>
-        <p className="lead">Hiba történt a hálózati kapcsolatban...</p>
-        <hr className="my-4" />
-        <p>
-          Próbálkozzon később újraindítani az oldalt vagy ellenőrízze az
-          internetkapcsolatot.
-        </p>
-        <p className="lead">
-          <RoutingButton text="Vissza a főoldalra" url="/" />
-        </p>
-      </div>
-    </React.Fragment>
+    <div className="jumbotron">
+      <h1 className="display-4">Hoppá!</h1>
+      <p className="lead">Hiba történt a hálózati kapcsolatban...</p>
+      <hr className="my-4" />
+      <p>
+        Próbálkozzon később újraindítani az oldalt vagy ellenőrízze az
+        internetkapcsolatot.
+      </p>
+      <p className="lead">
+        <RoutingButton text="Vissza a főoldalra" url="/" />
+      </p>
+    </div>
   );
 };
 
