dd8898ae394cec6fef3d6adbff501cbab7e760da
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-22 14:21:04 +0100

    Inline methods

--- src/components/common/tableBody.jsx
+++ src/components/common/tableBody.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 
 const TableBody = ({ collection, columns }) => {
-  const createKey = (item, column) => `${item.id}${column.value}`;
-
-  const renderCell = (item, column) => item[column.key];
-
   return (
     <tbody>
       {collection.map((item) => (
         <tr key={item.id}>
           {columns.map((column) => (
-            <td key={createKey(item, column)}>{renderCell(item, column)}</td>
+            <td key={`${item.id}${column.value}`}>{item[column.key]}</td>
           ))}
         </tr>
       ))}
