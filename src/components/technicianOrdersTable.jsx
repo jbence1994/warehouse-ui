f289af2ec38d7f66b95f5d6555b1d4fd1a1a3a24
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-22 13:59:15 +0100

    Fetch orders

--- src/components/technicianOrdersTable.jsx
+++ src/components/technicianOrdersTable.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Table from "./common/table";
 
+import { getTechnicianOrders } from "../services/technicianOrderService";
+
 const TechnicianOrdersTable = ({ technicianId }) => {
+  const [technicianOrders, setTechnicianOrders] = useState([]);
+
+  useEffect(() => {
+    const populateTechnicianOrders = async (id) => {
+      const { data } = await getTechnicianOrders(id);
+      setTechnicianOrders(data);
+    };
+
+    populateTechnicianOrders(technicianId);
+  }, [technicianId]);
+
   const columns = [
     { key: 1, value: "Azonosító" },
     { key: 2, value: "Dátum" },
     { key: 3, value: "Végösszeg" },
   ];
 
-  return <Table collection={[]} columns={columns} />;
+  return <Table collection={technicianOrders} columns={columns} />;
 };
 
 export default TechnicianOrdersTable;
