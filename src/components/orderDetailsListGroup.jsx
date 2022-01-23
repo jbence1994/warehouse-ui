568cbf514c7a036cdaa88c8b748aad009041d1c2
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-23 14:47:10 +0100

    Display total price

--- src/components/orderDetailsListGroup.jsx
+++ src/components/orderDetailsListGroup.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ListGroup from "./common/listGroup";
 
 const OrderDetailsListGroup = ({ collection }) => {
+  const [total, setTotal] = useState(0);
+
   return (
     <div className="form-group">
       <h5>Rendelés részletei</h5>
       <ListGroup collection={collection} />
+      <h4 className="m-3">Végösszeg: {total}.- Forint</h4>
     </div>
   );
 };
