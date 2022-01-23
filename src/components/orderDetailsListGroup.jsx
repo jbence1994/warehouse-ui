604f619a717e7e75a8538d6ab35aba4bece0ccf5
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-01-23 18:53:58 +0100

    Test rendering list group

--- src/components/orderDetailsListGroup.jsx
+++ src/components/orderDetailsListGroup.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import ListGroup from "./common/listGroup";
+// import ListGroup from "./common/listGroup";
 
 const OrderDetailsListGroup = ({ collection }) => {
   const [total, setTotal] = useState(0);
@@ -8,9 +8,26 @@ const OrderDetailsListGroup = ({ collection }) => {
   return (
     <div className="form-group">
       <h5>Rendelés részletei</h5>
-      <ListGroup collection={collection} />
+      <ul className="list-group">
+        {collection.map(({ productId, quantity }) => (
+          <li key={productId} className="list-group-item">
+            {`${productId} x ${quantity} darab`}
+          </li>
+        ))}
+      </ul>
+      {/*<ListGroup
+        collection={[
+          { key: "", value: "" },
+          { key: "", value: "" },
+          { key: "", value: "" },
+          { key: "", value: "" },
+          { key: "", value: "" },
+          { key: "", value: "" },
+        ]}
+      />*/}
       <h4 className="m-3">Végösszeg: {total}.- Forint</h4>
     </div>
   );
 };
+
 export default OrderDetailsListGroup;
