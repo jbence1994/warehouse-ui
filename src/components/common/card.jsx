f0cb70a64994f6b2c49365d2ddf3dd1f954462eb
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-02-01 14:24:48 +0100

    Add file (image) upload capability

--- src/components/common/card.jsx
+++ src/components/common/card.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 
-const Card = ({ title, imageSource, imageAlt, contents }) => {
+const Card = ({ title, imageSource, imageAlt, contents, onChange }) => {
   return (
     <div className="card mt-3 mb-3">
-      <img
-        src={imageSource}
-        className="card-img-top"
-        style={{
-          width: "250px",
-          height: "250px",
-          display: "block",
-          margin: "0 auto",
-          objectFit: "fill",
-        }}
-        alt={imageAlt}
-      />
+      <label htmlFor="photo-upload">
+        <img
+          src={imageSource}
+          className="card-img-top"
+          style={{
+            width: "250px",
+            height: "250px",
+            display: "block",
+            margin: "0 auto",
+            objectFit: "fill",
+          }}
+          alt={imageAlt}
+        />
+        <input
+          type="file"
+          id="photo-upload"
+          className="form-control-file"
+          onChange={onChange}
+          style={{
+            position: "absolute",
+            inset: "0",
+            opacity: "0",
+          }}
+        />
+      </label>
       <div className="card-body">
         <h5 className="card-title">{title}</h5>
         {contents.map((content) => (
