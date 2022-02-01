2b4e46af4d2f53b022434f4b13de650bf2393a4c
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-02-01 14:09:22 +0100

    Refactor React.Fragment to Fragment

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
 
 const Table = ({ caption, collection, columns }) => {
   return (
-    <React.Fragment>
+    <Fragment>
       <h5>{caption}</h5>
       <table className="table table-responsive table-hover text-center">
         <TableHeader columns={columns} />
         <TableBody collection={collection} columns={columns} />
       </table>
-    </React.Fragment>
+    </Fragment>
   );
 };
 
