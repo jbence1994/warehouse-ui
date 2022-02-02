b9b4cc3c3c21590a8cadda2be3564ccf24cda455
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-02-02 13:02:59 +0100

    Crate AddOnInput component

---
+++ src/components/common/addOnInput.jsx
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+
+const AddOnInput = ({
+  type,
+  placeholder = "",
+  name,
+  minValue,
+  addOnText,
+  errorMessage,
+  onChange,
+}) => {
+  return (
+    <Fragment>
+      <div className="input-group mb-3">
+        <input
+          type={type}
+          placeholder={placeholder}
+          name={name}
+          min={minValue}
+          className="form-control"
+          onChange={onChange}
+        />
+        <span className="input-group-text">{addOnText}</span>
+      </div>
+      {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+    </Fragment>
+  );
+};
+
+export default AddOnInput;
--- src/components/common/counter.jsx
+++ src/components/common/counter.jsx
@@ -1,24 +1,19 @@
 import React, { Fragment } from "react";
 
-// TODO: generalize this to a reusable component.
+import AddOnInput from "./addOnInput";
 
 // TODO: Separate this to an input group and a SupplyCounter.
 
 const Counter = ({ name, minValue, addOnText, errorMessage, onChange }) => {
   return (
-    <Fragment>
-      <div className="input-group mb-3">
-        <input
-          type="number"
-          name={name}
-          min={minValue}
-          className="form-control"
-          onChange={onChange}
-        />
-        <span className="input-group-text">{addOnText}</span>
-      </div>
-      {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-    </Fragment>
+    <AddOnInput
+      type="number"
+      name={name}
+      minValue={1}
+      addOnText={addOnText}
+      errorMessage={errorMessage}
+      onChange={onChange}
+    />
   );
 };
 
