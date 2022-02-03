3d01277ba0510648dd70ac3ac606db6075305d61
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-02-03 15:45:32 +0100

    Refactor step 3: Remove 'x' button from modal header

--- src/components/common/modalHeader.jsx
+++ src/components/common/modalHeader.jsx
@@ -4,14 +4,6 @@ const ModalHeader = ({ title }) => {
   return (
     <div className="modal-header">
       <h5 className="modal-title">{title}</h5>
-      <button
-        type="button"
-        className="close"
-        data-dismiss="modal"
-        aria-label="Close"
-      >
-        <span aria-hidden="true">&times;</span>
-      </button>
     </div>
   );
 };
