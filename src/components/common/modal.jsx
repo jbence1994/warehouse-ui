572dfaa72efe2c913703241939ee45c0a9cf1c50
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-02-05 11:07:22 +0100

    Centering the modal to the center of the screen

--- src/components/common/modal.jsx
+++ src/components/common/modal.jsx
@@ -11,32 +11,47 @@ const Modal = ({
   onClick,
 }) => {
   return (
-    <div className="modal-dialog modal-content">
-      <div className="modal-header">
-        <h5 className="modal-title">{title}</h5>
-      </div>
-      <div className="modal-body">
-        <p>{content}</p>
-      </div>
-      <div className="modal-footer">
-        {isSuccessModal ? (
-          <Fragment>
-            <button type="button" className="btn btn-primary" onClick={onClick}>
+    <div
+      className="modal-dialog"
+      style={{
+        position: "absolute",
+        float: "left",
+        left: "50%",
+        top: "50%",
+        transform: " translate(-50%, -50%)",
+      }}
+    >
+      <div className=" modal-content">
+        <div className="modal-header">
+          <h5 className="modal-title">{title}</h5>
+        </div>
+        <div className="modal-body">
+          <p>{content}</p>
+        </div>
+        <div className="modal-footer">
+          {isSuccessModal ? (
+            <Fragment>
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={onClick}
+              >
+                {buttonText}
+              </button>
+              <Link
+                className="btn btn-secondary"
+                data-dismiss="modal"
+                to={redirectUrl}
+              >
+                {routingButtonText}
+              </Link>
+            </Fragment>
+          ) : (
+            <button type="button" className="btn btn-danger" onClick={onClick}>
               {buttonText}
             </button>
-            <Link
-              className="btn btn-secondary"
-              data-dismiss="modal"
-              to={redirectUrl}
-            >
-              {routingButtonText}
-            </Link>
-          </Fragment>
-        ) : (
-          <button type="button" className="btn btn-danger" onClick={onClick}>
-            {buttonText}
-          </button>
-        )}
+          )}
+        </div>
       </div>
     </div>
   );
