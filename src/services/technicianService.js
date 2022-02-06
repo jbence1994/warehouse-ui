f6544a017b59bb59e4136fec632266e377ddc132
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-02-06 12:28:47 +0100

    Create a method to get all technicians

--- src/services/technicianService.js
+++ src/services/technicianService.js
@@ -2,12 +2,18 @@ import restClient from "axios";
 
 import endpoints from "../config/api.endpoints";
 
-const getTechnician = async (id) => {
-  const { API_ROOT, TECHNICIANS } = endpoints;
+const { API_ROOT, TECHNICIANS } = endpoints;
 
+const getTechnicians = async () => {
+  const endpointToCall = `${API_ROOT}${TECHNICIANS}`;
+
+  return await restClient.get(endpointToCall);
+};
+
+const getTechnician = async (id) => {
   const endpointToCall = `${API_ROOT}${TECHNICIANS}/${id}`;
 
   return await restClient.get(endpointToCall);
 };
 
-export { getTechnician };
+export { getTechnicians, getTechnician };
