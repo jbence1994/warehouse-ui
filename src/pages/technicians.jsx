edf74ab7985f960b64774b1d9a178498a66c7a0e
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-02-06 12:46:52 +0100

    Add structural elements to the page

--- src/pages/technicians.jsx
+++ src/pages/technicians.jsx
@@ -5,13 +5,15 @@ const Technicians = () => {
   const technicians = [1, 2, 3, 4];
 
   return (
-    <ul>
-      {technicians.map((id) => (
-        <li key={id}>
-          <Link to={`technikus/${id}`}>{id}</Link>
-        </li>
-      ))}
-    </ul>
+    <section className="row">
+      <article className="col-12">
+        {technicians.map((id) => (
+          <li key={id}>
+            <Link to={`technikus/${id}`}>{id}</Link>
+          </li>
+        ))}
+      </article>
+    </section>
   );
 };
 
