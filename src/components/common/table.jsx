5350c79636781e90582c3d7454cb49c49acd5df1
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-02-06 11:54:20 +0100

    Merge tables

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -1,15 +1,26 @@
 import React, { Fragment } from "react";
 
-import TableHeader from "./tableHeader";
-import TableBody from "./tableBody";
-
 const Table = ({ caption, columns, collection }) => {
   return (
     <Fragment>
       <h5>{caption}</h5>
       <table className="table table-responsive table-hover text-center">
-        <TableHeader columns={columns} />
-        <TableBody collection={collection} columns={columns} />
+        <thead>
+          <tr>
+            {columns.map((column) => (
+              <th key={column.key}>{column.value}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {collection.map((item) => (
+            <tr key={item.id}>
+              {columns.map((column) => (
+                <td key={`${item.id}${column.value}`}>{item[column.key]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
       </table>
     </Fragment>
   );
