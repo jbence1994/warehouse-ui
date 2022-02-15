44b22fbfbd862ceff41daa847ffbe3914c4d234c
Bence Juhasz <juhasz.bence.zsolt@gmail.com>
2022-02-15 12:05:35 +0100

    Remove dateService

--- src/services/dateService.js
+++
@@ -1,9 +0,0 @@
-import moment from "moment";
-import "moment/locale/hu";
-
-const getCurrentDate = () => {
-  const momentObj = moment();
-  return `${momentObj.format("LL")}, ${momentObj.format("dddd")}`;
-};
-
-export default getCurrentDate;
