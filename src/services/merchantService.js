61dba18e1d968822cab20cadda766719b40042af
Bence Juhasz <33096954+jbence1994@users.noreply.github.com>
2022-02-17 11:49:17 +0100

    [No Ticket] - Clean up destructures in services (#65)

--- src/services/merchantService.js
+++ src/services/merchantService.js
@@ -2,17 +2,15 @@ import restClient from "axios";
 
 import endpoints from "../config/api.endpoints";
 
-const getMerchantKeyValuePairs = async () => {
-  const { API_ROOT, MERCHANTS, AS_KEY_VALUE_PAIR } = endpoints;
+const { API_ROOT, MERCHANTS, AS_KEY_VALUE_PAIR } = endpoints;
 
+const getMerchantKeyValuePairs = async () => {
   const endpointToCall = `${API_ROOT}${MERCHANTS}${AS_KEY_VALUE_PAIR}`;
 
   return await restClient.get(endpointToCall);
 };
 
 const createMerchant = async (merchant) => {
-  const { API_ROOT, MERCHANTS } = endpoints;
-
   const endpointToCall = `${API_ROOT}${MERCHANTS}`;
 
   return await restClient.post(endpointToCall, merchant);
